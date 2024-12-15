5b1a3842f01e783f5a8a084fd9d28837718fdeb1
jaibwii <jbrwoj32@gmail.com>
2024-12-15 23:35:00 +0800

    Update Home Page

--- src/app/admin/home/home.component.spec.ts
+++ src/app/admin/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { DataBindingDemoComponent } from '../../data-binding-demo/data-binding-demo.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +9,7 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
+      imports: [HomeComponent, DataBindingDemoComponent]
     })
     .compileComponents();
 
--- src/app/admin/home/home.component.ts
+++ src/app/admin/home/home.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { DataBindingDemoComponent } from "../../data-binding-demo/data-binding-demo.component";
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  username = "Jb Reyes"
+  description = "You may now use our applications in this website."
 
+  textColor='blue';
+  textColor2='red';
+  textColor3='yellow';
+
+  myStory = "";
+
+  count = 0;
+  increment() {
+    this.count++;
+  }
+  decrement() {
+    this.count--;
+  }
+
+  devices = ['Smartwatch','Games console','Smart home device'];
 }
